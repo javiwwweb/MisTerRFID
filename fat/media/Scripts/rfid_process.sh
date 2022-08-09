023806875490d929a50ae6b5c60de2827d3ff9a2
javiwwweb <javiwwweb@gmail.com>
2022-08-09 18:17:30 -0400

    Update rfid_process.sh
    added sample UID and MRA files with their proper syntax

--- fat/media/Scripts/rfid_process.sh
+++ fat/media/Scripts/rfid_process.sh
@@ -17,12 +17,22 @@ qload_rom()
 }
 
 case "$1" in 
-   "noscan") qload_rom Atari2600 noscan.rom "$1";;   
-   "4034012226") qload_rom NES Anticipation.nes "$1";;
-   "3826459960") qload_rom NES BubbleBobble.nes "$1";;
-   "3978110274") qload_rom NES Castlevania.nes "$1";;   
-   "1826759746") qload_rom SNES Aladdin.sfc "$1";;   
-   "1107764546") qload_rom SNES DK1.sfc "$1";;
-   "2398168120") qload_rom SNES DK2.sfc "$1";;
-   "3921617986") qload_rom SNES DK3.sfc "$1";;   
+   "2208192798") echo load_core /media/fat/_Arcade/'Robocop (World revision 4).mra' > /dev/MiSTer_cmd;;
+   "1944463134") echo load_core /media/fat/_Arcade/'Final Fight -World, set 1-.mra' > /dev/MiSTer_cmd;;
+   "2212386078") echo load_core /media/fat/_Arcade/'Ghosts'\''n Goblins (JT).mra' > /dev/MiSTer_cmd;;
+   "3841642020") echo load_core /media/fat/_Arcade/'Double Dragon.mra' > /dev/MiSTer_cmd;;
+   "2788154149") echo load_core /media/fat/_Arcade/'Double Dragon II - The Revenge.mra' > /dev/MiSTer_cmd;;
+   "2785325093") echo load_core /media/fat/_Arcade/'Kung-Fu Master.mra' > /dev/MiSTer_cmd;;
+   "2532548645") echo load_core /media/fat/_Arcade/'Shinobi (Set 6, World, S16A) [No Protection].mra' > /dev/MiSTer_cmd;;
+   "3571898404") echo load_core /media/fat/_Arcade/'R-Type (World).mra' > /dev/MiSTer_cmd;;
+   "3836830500") echo load_core /media/fat/_Arcade/'Vigilante (World, Rev E).mra' > /dev/MiSTer_cmd;;
+   "4095029796") echo load_core /media/fat/_Arcade/'Bad Dudes vs. Dragonninja (US revision 1).mra' > /dev/MiSTer_cmd;;
+   "4100998436") echo load_core /media/fat/_Arcade/"Ghouls'n Ghosts -World-.mra" > /dev/MiSTer_cmd;;
+   "4095293732") echo load_core /media/fat/_Arcade/'Rush'\''n Attack (US).mra' > /dev/MiSTer_cmd;;
+   "4097148196") echo load_core /media/fat/_Arcade/'Altered Beast (set 8) (8751 317-0078).mra' > /dev/MiSTer_cmd;;
+   "3841431588") echo load_core /media/fat/_Arcade/'Golden Axe (set 6, US) (8751 317-123A).mra' > /dev/MiSTer_cmd;;
+   "3570877988") echo load_core /media/fat/_Arcade/"Street Fighter II'  Hyper Fighting -World 921209-.mra" > /dev/MiSTer_cmd;;
+   "2793426725") echo load_core /media/fat/_Arcade/'Rastan (World Rev 1).mra' > /dev/MiSTer_cmd;;
+   "2790045733") qload_rom NEOGEO mslugx "$1";;   
+   "3837547300") qload_rom NEOGEO samsho2 "$1";;  
 esac
